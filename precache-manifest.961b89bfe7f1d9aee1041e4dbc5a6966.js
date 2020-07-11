afc8d410e6e088c53b2781b628f790f96cab8a28
Darth-koder007 <vj2010sn@gmail.com>
2020-07-11 16:22:15 +0000

    Deploying to gh-pages from  @ 7f20d935ee3b22aa9c3562c887e175a4c039a8bc 🚀

---
+++ precache-manifest.961b89bfe7f1d9aee1041e4dbc5a6966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa4eb8ee85f012ccd176ed90763940b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47acb1fed718bce7418e",
+    "url": "/static/css/main.92e37e7c.chunk.css"
+  },
+  {
+    "revision": "81a101037fb1bfd93579",
+    "url": "/static/js/2.7f3eccb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f3eccb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47acb1fed718bce7418e",
+    "url": "/static/js/main.52f894a8.chunk.js"
+  },
+  {
+    "revision": "875656897a7fcd06be95",
+    "url": "/static/js/runtime-main.abcb49c9.js"
+  }
+]);
