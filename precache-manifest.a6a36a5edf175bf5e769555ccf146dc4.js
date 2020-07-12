cbf92e5123f524c3ed12bb706c31a88b9032deb7
Darth-koder007 <vj2010sn@gmail.com>
2020-07-12 11:42:45 +0000

    Deploying to gh-pages from  @ 50441199df803c91433638152f514ee511ed7c3f 🚀

---
+++ precache-manifest.a6a36a5edf175bf5e769555ccf146dc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7371cd5530fe1bf92e7c0a142556209",
+    "url": "/react-tube/index.html"
+  },
+  {
+    "revision": "9e5fc11a85bc15c1960a",
+    "url": "/react-tube/static/css/main.14045f52.chunk.css"
+  },
+  {
+    "revision": "d6daa2b4131709bc4a7a",
+    "url": "/react-tube/static/js/2.cb2aec9e.chunk.js"
+  },
+  {
+    "revision": "0168ae5ee7e4c241bfcc95b4e4bf430e",
+    "url": "/react-tube/static/js/2.cb2aec9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5fc11a85bc15c1960a",
+    "url": "/react-tube/static/js/main.26168d84.chunk.js"
+  },
+  {
+    "revision": "56573b9803a987fa22e1",
+    "url": "/react-tube/static/js/runtime-main.fd747b89.js"
+  },
+  {
+    "revision": "d289251a483291fe68857338e7cb69e2",
+    "url": "/react-tube/static/media/youtube.d289251a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.961b89bfe7f1d9aee1041e4dbc5a6966.js"
+  "/react-tube/precache-manifest.a6a36a5edf175bf5e769555ccf146dc4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tube/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
