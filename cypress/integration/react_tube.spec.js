a75505b90a8cc24f0ae9554c99c43790755c08ed
Darth-koder007 <vj2010sn@gmail.com>
2020-07-15 00:31:34 +0530

    ready to implement

--- cypress/integration/react_tube.spec.js
+++ cypress/integration/react_tube.spec.js
@@ -1,79 +1,13 @@
 describe("React tube testing", () => {
-  beforeEach(() => {
-    cy.server();
+  beforeEach(() => {});
 
-    cy.route("GET", "**react-tube/youtube/v3/search**", "fx:data.json").as(
-      "getVideos"
-    );
-  });
+  it("Should contain search bar", () => {});
 
-  it("Should contain search bar", () => {
-    cy.visit("/");
+  it("Should show loader after user has typed in searchbar", () => {});
 
-    cy.findByTestId("search-bar-container").should("exist");
-  });
+  it("Should show video detail", () => {});
 
-  it("Should show loader after user has typed in searchbar", () => {
-    cy.visit("/");
+  it("should show video list items correctly", () => {});
 
-    cy.findByTestId("search-bar-container").type("football");
-
-    cy.findByTestId("loader").should("exist");
-  });
-
-  it("Should show video detail", () => {
-    cy.visit("/");
-
-    cy.findByTestId("search-bar-container").type("football");
-
-    cy.findByTestId("video-detail-container").should("exist");
-  });
-
-  it("should show video list items correctly", () => {
-    cy.visit("/");
-
-    cy.findByTestId("search-bar-container").type("football");
-    cy.wait("@getVideos").then((xhr) => {
-      const { items } = xhr.responseBody;
-
-      cy.findAllByTestId("video-list-item-container")
-        .should("have.length", items.length)
-        .each(($videoListItem, index) => {
-          const videoListItem = items[index];
-
-          cy.wrap($videoListItem).within(() => {
-            cy.findByRole("img").should(
-              "have.attr",
-              "src",
-              videoListItem.snippet.thumbnails.default.url
-            );
-
-            cy.findByText(videoListItem.snippet.title, {
-              selector: "p",
-            }).should("exist");
-          });
-        });
-    });
-  });
-
-  it("should show selected video ", () => {
-    cy.visit("/");
-
-    cy.findByTestId("search-bar-container").type("football");
-
-    cy.wait("@getVideos").then((xhr) => {
-      const { items } = xhr.responseBody;
-      const videoListItem = items[items.length - 1];
-      cy.findAllByTestId("video-list-item-container").then(
-        ($videoListItemContainer) => {
-          $videoListItemContainer[items.length - 1].click();
-        }
-      );
-      cy.findByTestId("video-detail-container")
-        .findByText(videoListItem.snippet.title, {
-          selector: "div",
-        })
-        .should("exist");
-    });
-  });
+  it("should show selected video ", () => {});
 });
