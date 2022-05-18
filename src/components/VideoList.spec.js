09d08e95392e78881c6404d2fef7c791798300ef
Vijay Singh <vj2010sn@gmail.com>
2022-05-18 11:19:12 +0200

    Video list spec (#14)
    * add coverage for videolist
    
    * vide list test
    
    * video list spec more coverage
    
    * changed ci
    
    * decrease coverage
    
    * decrease coverage
    
    * updated workflow

--- src/components/VideoList.spec.js
+++ src/components/VideoList.spec.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import VideoListItem from "./VideoListItem";
 
 const video = {
@@ -17,4 +17,25 @@ test("should match video list", () => {
   render(<VideoListItem video={video} key={video.etag} setSelectVideo={fn} />);
   const videoListEl = screen.getByText(video.snippet.title);
   expect(videoListEl).toBeInTheDocument();
+});
+
+test("should call the callback for video list on clicking", () => {
+  render(<VideoListItem video={video} key={video.etag} setSelectVideo={fn} />);
+  const videoListEl = screen.getByText(video.snippet.title);
+  fireEvent.click(videoListEl);
+  expect(fn).toHaveBeenCalled();
+});
+
+test("should call the callback for video list on clicking with specific number of time", () => {
+  render(<VideoListItem video={video} key={video.etag} setSelectVideo={fn} />);
+  const videoListEl = screen.getByText(video.snippet.title);
+  fireEvent.click(videoListEl);
+  expect(fn).toHaveBeenCalledTimes(1);
+});
+
+test("should call the callback for video list on clicking with specific argument", () => {
+  render(<VideoListItem video={video} key={video.etag} setSelectVideo={fn} />);
+  const videoListEl = screen.getByText(video.snippet.title);
+  fireEvent.click(videoListEl);
+  expect(fn).toHaveBeenCalledWith(video);
 });
